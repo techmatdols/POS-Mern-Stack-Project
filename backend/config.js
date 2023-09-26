bce8b5659b600d19f261c49e9d4b7c34950b8e3f
techmatdols <mudoh160@gmail.com>
2023-09-26 18:18:18 +0100

    booking-app

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://mudoh160:giovanni23@cluster0.yqwxx4d.mongodb.net/cluster0?retryWrites=true&w=majority"
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
